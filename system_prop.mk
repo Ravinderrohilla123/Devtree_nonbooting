a0472a9c544f3d329964fb91113628bc8a6ea29b
Ravinderrohilla123 <153418233+Ravinderrohilla123@users.noreply.github.com>
2023-12-11 18:14:42 +0530

    ....tried for some bt..connections

--- system_prop.mk
+++ system_prop.mk
@@ -26,22 +26,22 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Bluetooth
 PRODUCT_PRODUCT_PROPERTIES += \
-    bluetooth.device.class_of_device?=90,2,12 \
+    bluetooth.device.class_of_device=90,2,12 \
     bluetooth.hardware.power.operating_voltage_mv=3300 \
-    bluetooth.profile.asha.central.enabled?=true \
-    bluetooth.profile.avrcp.target.enabled?=true \
-    bluetooth.profile.a2dp.source.enabled?=true \
-    bluetooth.profile.bas.client.enabled?=true \
-    bluetooth.profile.gatt.enabled?=true \
-    bluetooth.profile.hfp.ag.enabled?=true \
-    bluetooth.profile.hid.device.enabled?=true \
-    bluetooth.profile.hid.host.enabled?=true \
-    bluetooth.profile.map.server.enabled?=true \
-    bluetooth.profile.opp.enabled?=true \
-    bluetooth.profile.pan.nap.enabled?=true \
-    bluetooth.profile.pan.panu.enabled?=true \
-    bluetooth.profile.pbap.server.enabled?=true \
-    bluetooth.profile.sap.server.enabled?=true
+    bluetooth.profile.asha.central.enabled=true \
+    bluetooth.profile.avrcp.target.enabled=true \
+    bluetooth.profile.a2dp.source.enabled=true \
+    bluetooth.profile.bas.client.enabled=true \
+    bluetooth.profile.gatt.enabled=true \
+    bluetooth.profile.hfp.ag.enabled=true \
+    bluetooth.profile.hid.device.enabled=true \
+    bluetooth.profile.hid.host.enabled=true \
+    bluetooth.profile.map.server.enabled=true \
+    bluetooth.profile.opp.enabled=true \
+    bluetooth.profile.pan.nap.enabled=true \
+    bluetooth.profile.pan.panu.enabled=true \
+    bluetooth.profile.pbap.server.enabled=true \
+    bluetooth.profile.sap.server.enabled=true
 
 PRODUCT_SYSTEM_PROPERTIES += \
     persist.bluetooth.system_audio_hal.enabled=1
